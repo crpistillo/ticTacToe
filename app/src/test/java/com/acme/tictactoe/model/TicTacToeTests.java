002aaadb68a550ff7ef348f04362313e1c55d763
Agustin Rojas <agustin.rojas@despegar.com>
2020-05-24 18:36:31 -0300

    Clean code

--- app/src/test/java/com/acme/tictactoe/model/TicTacToeTests.java
+++ app/src/test/java/com/acme/tictactoe/model/TicTacToeTests.java
@@ -1,7 +1,5 @@
 package com.acme.tictactoe.model;
 
-import android.os.Build;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,57 +20,57 @@ public class TicTacToeTests {
 
     /**
      * This test will simulate and verify x is the winner.
-     *
-     *    X | X | X
-     *    O |   |
-     *      | O |
+     * <p>
+     * X | X | X
+     * O |   |
+     * | O |
      */
     @Test
     public void test3inRowAcrossTopForX() {
 
-        board.mark(0,0); // x
+        board.mark(0, 0); // x
         assertNull(board.getWinner());
 
-        board.mark(1,0); // o
+        board.mark(1, 0); // o
         assertNull(board.getWinner());
 
-        board.mark(0,1); // x
+        board.mark(0, 1); // x
         assertNull(board.getWinner());
 
-        board.mark(2,1); // o
+        board.mark(2, 1); // o
         assertNull(board.getWinner());
 
-        board.mark(0,2); // x
+        board.mark(0, 2); // x
         assertEquals(Player.X, board.getWinner());
     }
 
 
     /**
      * This test will simulate and verify o is the winner.
-     *
-     *    O | X | X
-     *      | O |
-     *      | X | O
+     * <p>
+     * O | X | X
+     * | O |
+     * | X | O
      */
     @Test
     public void test3inRowDiagonalFromTopLeftToBottomForO() {
 
-        board.mark(0,1); // x
+        board.mark(0, 1); // x
         assertNull(board.getWinner());
 
-        board.mark(0,0); // o
+        board.mark(0, 0); // o
         assertNull(board.getWinner());
 
-        board.mark(2,1); // x
+        board.mark(2, 1); // x
         assertNull(board.getWinner());
 
-        board.mark(1,1); // o
+        board.mark(1, 1); // o
         assertNull(board.getWinner());
 
-        board.mark(0,2); // x
+        board.mark(0, 2); // x
         assertNull(board.getWinner());
 
-        board.mark(2,2); // o
+        board.mark(2, 2); // o
         assertEquals(Player.O, board.getWinner());
 
     }
